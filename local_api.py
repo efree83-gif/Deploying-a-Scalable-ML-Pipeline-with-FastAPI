ebc376d9b5d0109cae0c41a19339eb3f734c3ba5
Eric Freeman <freeman.e.l@gmail.com>
2026-09-13 16:47:39 -0700

    Update local_api.py POST endpoint with trailing slash

--- local_api.py
+++ local_api.py
@@ -27,7 +27,7 @@ data = {
 }
 
 # TODO: send a POST using the data above
-r = requests.post("http://127.0.0.1:8000/data", json=data)
+r = requests.post("http://127.0.0.1:8000/data/", json=data)
 
 # TODO: print the status code
 print(f"POST Status: {r.status_code}")
